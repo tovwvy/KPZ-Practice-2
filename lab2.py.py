6b15bc8a030182c3c2249c2a6ff808084548edfe
tovwvy <104623411+tovwvy@users.noreply.github.com>
2022-05-06 12:32:15 +0300

    Update lab2.py.py

--- lab2.py.py
+++ lab2.py.py
@@ -1,27 +1,14 @@
-primes = []
-for target_num in range(100, 222):
-  is_prime = True
-  for i in range(2, target_num):
-    if target_num % i == 0:
-      is_prime = False
-  if is_prime:
-    primes.append(target_num)
-
-print(primes)
-
-
-def is_prime(num):
-    for i in range(2,(num // 2) + 1):
-        if num % i == 0:
-            return False
-    return True
-
-
 def prime_num_generator():
-    num = 2
-    while True:
-        if is_prime(num):
-            yield num
-            num += 1
-        else:
-            num += 1
+    for n in range(2, 666): 
+        for x in range(2, n):  
+            if n % x == 0:  
+                break
+        else:  
+            yield n
+        n += 1
+
+
+a = prime_num_generator()
+
+  while True:
+    print(next(a))
